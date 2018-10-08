7f7bc454678bf543985cbd4ef14c755b8ea95320
emilyevans <emilyevans@wifi-roaming-128-4-104-235.host.udel.edu>
2018-10-08 02:48:36 -0400

    final commit

--- src/pkgCore/Deck.java
+++ src/pkgCore/Deck.java
@@ -48,10 +48,10 @@ public class Deck {
 	public int getRemaining(Object eNum) {
 		int count = 0;
 		for (Card card: cardsInDeck) { 
-			if (card.geteSuit() == eNum && eNum instanceof eSuit) {
+			if (card.geteSuit() == eNum) {
 				count ++;
 			}
-			else if (card.geteRank() == eNum && eNum instanceof eRank) {
+			else if (card.geteRank() == eNum) {
 				count ++;
 			}
 		}
